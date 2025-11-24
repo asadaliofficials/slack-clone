c7d27a644d999ac4b3d2f5cefd7a803908eb6d75
Asad Ali <message.asadali@gmail.com>
2025-11-24 11:49:39 +0500

    clerk auth setup in frontend

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -1,5 +1,22 @@
-import express from 'express'
+import express from 'express';
+import { clerkMiddleware } from '@clerk/express';
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(clerkMiddleware());
+
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+
+app.get('/health', (req, res) => {
+	res.json({
+		status: 'ok',
+		statusCode: 200,
+		message: 'Healthy',
+	});
+});
 
 export default app;
