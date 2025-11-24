2454f541f9156eceffdeb782b1ef9ec26f5c1347
Asad Ali <message.asadali@gmail.com>
2025-11-24 13:42:22 +0500

    db multiple connection issue fixed

--- BACKEND/src/config/db.config.js
+++ BACKEND/src/config/db.config.js
@@ -2,11 +2,15 @@ import mongoose from 'mongoose';
 import { MONGODB_URI } from './env.config.js';
 
 const connectDB = async () => {
+	if (mongoose.connection.readyState === 1) {
+		// Already connected
+		return;
+	}
 	try {
 		await mongoose.connect(MONGODB_URI);
 		console.log('Database connected');
 	} catch (error) {
-		console.log('Failed to conntect with DB!');
+		console.log('Failed to connect with DB!');
 	}
 };
 
