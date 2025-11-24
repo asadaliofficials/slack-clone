1fe7e804ea7c84728df7b16cab16995035e78221
Asad Ali <message.asadali@gmail.com>
2025-11-24 12:44:47 +0500

    auth setup with clerk, mongodb and inngest is completed

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import { clerkMiddleware } from '@clerk/express';
+import { serve } from "inngest/express";
+import { functions, inngest } from './config/inngest.config.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(clerkMiddleware());
+
+app.use('/api/inngest', serve({client: inngest, functions}))
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
