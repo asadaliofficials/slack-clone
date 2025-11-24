c05f71d30a6a68d0a0cf03094f536deb304c4fad
Asad Ali <message.asadali@gmail.com>
2025-11-24 09:31:19 +0500

    added env vars

--- BACKEND/src/config/env.config.js
+++ BACKEND/src/config/env.config.js
@@ -2,4 +2,15 @@ import { config } from 'dotenv';
 
 config();
 
-export const { PORT, MONGODB_URI, NODE_ENV } = process.env;
+export const {
+	PORT,
+	MONGODB_URI,
+	NODE_ENV,
+	CLERK_PUBLISHABLE_KEY,
+	CLERK_SECRET_KEY,
+	SLACK_API_KEY,
+	SLACK_API_SECRET,
+	SENTRY_DSN,
+	INJEST_EVENT_KEY,
+	INJEST_SIGNING_KEY,
+} = process.env;
