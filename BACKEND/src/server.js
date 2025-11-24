3c86334686f64b0f085470c87561d3b6a268693d
Asad Ali <message.asadali@gmail.com>
2025-11-24 13:16:19 +0500

    followed vercel serverless structure rules

--- BACKEND/src/server.js
+++ BACKEND/src/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import { clerkMiddleware } from '@clerk/express';
-import { serve } from "inngest/express";
+import { serve } from 'inngest/express';
 import { functions, inngest } from './config/inngest.config.js';
+import connectDB from './config/db.config.js';
+import { NODE_ENV, PORT } from './config/env.config.js';
 
 const app = express();
 
@@ -9,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(clerkMiddleware());
 
-app.use('/api/inngest', serve({client: inngest, functions}))
+app.use('/api/inngest', serve({ client: inngest, functions }));
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
@@ -22,5 +24,21 @@ app.get('/health', (req, res) => {
 		message: 'Healthy',
 	});
 });
+
+const startServer = () => {
+	try {
+		connectDB();
+		if (NODE_ENV === 'development') {
+			app.listen(PORT, () => {
+				console.log('Server listening on port', PORT);
+			});
+		}
+	} catch (error) {
+		console.log(error);
+		process.exit(1);
+	}
+};
+
+startServer();
 
 export default app;
