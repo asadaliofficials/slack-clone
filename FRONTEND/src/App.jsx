d4d9fc2edcf1c262bdc62b8c2fa06cb88ea95895
Asad Ali <asadjattofficials@gmail.com>
2025-11-29 12:24:50 +0500

    implemented frontend page and routes

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,15 +1,22 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
 import React from 'react';
+import { Route, Routes } from 'react-router';
+import HomePage from './pages/HomePage';
+import AuthPage from './pages/AuthPage';
 
 const App = () => {
 	return (
 		<header>
-			<SignedOut>
-				<SignInButton mode="modal" />
-			</SignedOut>
 			<SignedIn>
-				<UserButton />
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+				</Routes>
 			</SignedIn>
+			<SignedOut>
+				<Routes>
+					<Route path="*" element={<AuthPage />} />
+				</Routes>
+			</SignedOut>
 		</header>
 	);
 };
---
+++ FRONTEND/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import { UserButton } from '@clerk/clerk-react';
+import React from 'react';
+
+const HomePage = () => {
+	return (
+		<>
+			<div>HomePage</div>
+			<UserButton />
+		</>
+	);
+};
+
+export default HomePage;
