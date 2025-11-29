52f307aa3ae154e2ba085e6c69ba88f03a820e51
Asad Ali <message.asadali@gmail.com>
2025-11-29 13:51:46 +0500

    implemented stream connection in react

--- FRONTEND/src/pages/HomePage.jsx
+++ FRONTEND/src/pages/HomePage.jsx
@@ -1,7 +1,10 @@
 import { UserButton } from '@clerk/clerk-react';
 import React from 'react';
 
+import {chatclient, isloading, error} from '../hooks/useStreamChat'
+
 const HomePage = () => {
+
 	return (
 		<>
 			<div>HomePage</div>
