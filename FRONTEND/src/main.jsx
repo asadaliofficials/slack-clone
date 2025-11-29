f41129bb244c4e3df495b03f851e5f9d391eea8e
Asad Ali <message.asadali@gmail.com>
2025-11-29 12:24:50 +0500

    implemented frontend page and routes

--- FRONTEND/src/main.jsx
+++ FRONTEND/src/main.jsx
@@ -1,8 +1,8 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { ClerkProvider } from '@clerk/clerk-react';
+import { BrowserRouter } from 'react-router';
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
@@ -12,9 +12,9 @@ if (!PUBLISHABLE_KEY) {
 }
 
 createRoot(document.getElementById('root')).render(
-	<StrictMode>
-		<ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+	<ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+		<BrowserRouter>
 			<App />
-		</ClerkProvider>
-	</StrictMode>
+		</BrowserRouter>
+	</ClerkProvider>
 );
